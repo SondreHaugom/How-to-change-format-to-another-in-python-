81b401227709774dc3238fbca5939407038e1625
SondreHaugom <son3012@skole.telemarkfylke.no>
2024-11-27 12:10:18 +0100

    New comments and tabels

--- How to change format to another in python/changeFormat.py
+++ How to change format to another in python/changeFormat.py
@@ -1,34 +1,36 @@
 import openpyxl as op
-# henter datane fra excel formatet
+# retrieves the data from the excel format
 data = op.load_workbook(r"C:\Users\SondreHaugom\OneDrive - Telemark fylkeskommune\Sondre\Prakis oppgaver høst 2024\endreFromat\innbyggerliste-skien-test-20.11.2024.xlsx")
 sheets =  data.sheetnames
 sheet = data.active
 
-#oppretter ny Excel-fil for export
+# creates a new Excel file for export
 ny_fil = op.Workbook()
 ny_sheet = ny_fil.active
-ny_sheet.tittle = "Nytt format"
+ny_sheet.tittle = "New format with more tabels"
 
-# henter kolonner
-ny_sheet.append(["Fornavn","Etternavn","Adresse","Post nummer","Post sted"])
+# fetches columns
+ny_sheet.append(["Fornavn","Etternavn","Adresse","Post nummer","Post sted","Identifikasjonsnummer_foedselsEllerDNummer"])
 
-# finner max antal rader 
+# finds max number of rows
 rows = sheet.max_row
 
-# henter innformasjonen fra det første excel arket
+# retrieves the information from the first excel sheet
 for i in range(2, rows + 1):
     fornavn = sheet[f"D{i}"].value
     etternavn = sheet[f"E{i}"].value
     adresse = sheet[f"F{i}"].value
-    post_nummer = sheet[f"G{i}"].valu
+    post_nummer = sheet[f"G{i}"].value
     post_sted = sheet[f"H{i}"].value
+    Identifikasjonsnummer_foedselsEllerDNummer = sheet[f"I{i}"].value
 
-    if fornavn and etternavn and adresse and post_nummer and post_sted:
 
-        ny_sheet.append([fornavn,etternavn,adresse,post_nummer,post_sted])
+    if fornavn and etternavn and adresse and post_nummer and post_sted and Identifikasjonsnummer_foedselsEllerDNummer:
 
-# lager den nye excel filen
-output_path = r"C:\Users\SondreHaugom\OneDrive - Telemark fylkeskommune\Sondre\Prakis oppgaver høst 2024\endreFromat\Nytt_format.xlsx"
+        ny_sheet.append([fornavn,etternavn,adresse,post_nummer,post_sted,Identifikasjonsnummer_foedselsEllerDNummer])
+
+# create the new excel file
+output_path = r"C:\Users\SondreHaugom\OneDrive - Telemark fylkeskommune\Sondre\Prakis oppgaver høst 2024\endreFromat\New format with more tabels.xlsx"
 ny_fil.save(output_path)
 
 print(f"Data ekspoterer til {output_path}")
